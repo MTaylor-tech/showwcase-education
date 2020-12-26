68d0a1b693c667b6fe11f496d83ec56443baacc9
Matt Taylor <matt@mtaylor.tech>
2020-12-25 20:42:40 -0500

    a little styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Logo = styled.img`
+  display: inline;
+  margin: 10px 20px 10px 0;
+  width: 100px;
+  height: 100px;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const OverallContainer = styled.div`
+  display: flex;
+`;
 
 const Header = (props: any): JSX.Element => {
   return (
-    <React.Fragment>
+    <OverallContainer>
+      <Logo src="ShowwcasLogo100x100.png" />
       {props.loggedIn?
-        <div>
+        <HeaderContainer>
           <h1>{props.name}</h1>
           <h4>@{props.name.toLowerCase().split(' ').join('')}</h4>
           <p>{props.name}'s Education Showwcase</p>
-        </div>
+        </HeaderContainer>
         :
-        <div>
+        <HeaderContainer>
           <h1>Welcome</h1>
           <p>Welcome to your Education Showwcase. Please enter your name below to begin.</p>
-        </div>
+        </HeaderContainer>
       }
-    </React.Fragment>
+    </OverallContainer>
   );
 };
 
